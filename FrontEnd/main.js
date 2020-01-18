91e001f40b41f5622ba91d39b5939eb9b3c1808a
Vinicius <vincx343@gmail.com>
2020-01-18 17:09:16 -0300

    deleted old angular version

--- FrontEnd/main.js
+++
@@ -1,7 +0,0 @@
-angular.module('demo', [])
-.controller('Cadastro', function($scope, $http) {
-    $http.get('http://localhost:5000/cadastro').
-        then(function(response) {
-            $scope.empresas = response.data;
-        });
-});
