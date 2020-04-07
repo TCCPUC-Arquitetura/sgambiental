42f1dad1fb6619df9d066fcc8e40f10ef7e41472
douglasodro <douglasodro@gmail.com>
2020-04-07 20:57:07 -0300

    Correções JWT, ControllerLogin.

--- SGAmbientalTCC/Classes.cs
+++ SGAmbientalTCC/Classes.cs
@@ -10,6 +10,7 @@ namespace SGAmbientalTCC
     public class User
     {
         public string UserID { get; set; }
+        public string Password { get; set; }
         public string AccessKey { get; set; }
     }
 
