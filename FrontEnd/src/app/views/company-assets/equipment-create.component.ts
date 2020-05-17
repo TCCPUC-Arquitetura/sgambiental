5a278892e2c928f95fd9ea17cc13de1ee96f94e8
Vinicius Conceicao <vinc-analist@hotmail.com>
2020-05-17 15:31:52 -0300

    funcionalidade equipamentos

---
+++ FrontEnd/src/app/views/company-assets/equipment-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'equipment-create.component.html'
+})
+export class EquipmentCreateComponent {
+
+  constructor() { }
+
+}
